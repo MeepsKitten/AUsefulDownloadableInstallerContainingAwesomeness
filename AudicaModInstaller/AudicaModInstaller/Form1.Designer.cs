7d3bb9136547b4765a0aaf685d01b67fa0f675fe
MeepsKitten <meepsalot69@gmail.com>
2021-06-21 17:33:53 -0700

    Automatic MelonLoader Installation

---
+++ AudicaModInstaller/AudicaModInstaller/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AudicaModInstaller
+{
+    partial class Audica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Audica));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Install = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-102, -53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(663, 427);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Install
+            // 
+            this.Install.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Install.Location = new System.Drawing.Point(371, 340);
+            this.Install.Name = "Install";
+            this.Install.Size = new System.Drawing.Size(87, 22);
+            this.Install.TabIndex = 1;
+            this.Install.Text = "Install";
+            this.Install.UseVisualStyleBackColor = true;
+            this.Install.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(2, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Audica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 365);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Install);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Audica";
+            this.Text = "Audica Mod Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Install;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AudicaModInstaller/AudicaModInstaller/Form1.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace AudicaModInstaller
+{
+    public partial class Audica : Form
+    {
+        private List<string> steamGameDirs = new List<string>();
+
+        public Audica()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SearchSteam();
+            foreach(var dir in steamGameDirs)
+            {
+                var subDirs = Directory.GetDirectories(dir);
+                foreach(var subDir in subDirs)
+                {
+                    if(subDir.Contains("Audica"))
+                    {
+                        label1.Text = subDir;
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string MelonDLAddress = new WebClient().DownloadString("https://raw.githubusercontent.com/MeepsKitten/AUsefulDownloadableInstallerContainingAwesomeness/main/MelonVersion.txt");         
+            Uri uri = new Uri(MelonDLAddress);
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(MelonZipDownloadCallback);
+                client.DownloadFileAsync(uri, $"{label1.Text}\\melon.zip");
+            }
+        }
+
+        private void MelonZipDownloadCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            ZipArchive melonZip = ZipFile.Open($"{label1.Text}\\melon.zip", ZipArchiveMode.Read);
+
+            foreach(var entry in melonZip.Entries)
+            {
+                var filename = $"{label1.Text}\\{entry.FullName}";
+
+                string directory = Path.GetDirectoryName(filename);
+
+                if (!Directory.Exists(directory))
+                    Directory.CreateDirectory(directory);
+
+                if (entry.Name != "")
+                    entry.ExtractToFile(filename, true);
+            }           
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void SearchSteam()
+        {
+            steamGameDirs.Clear();
+            string steam32 = "SOFTWARE\\VALVE\\";
+            string steam64 = "SOFTWARE\\Wow6432Node\\Valve\\";
+            string steam32path;
+            string steam64path;
+            string config32path;
+            string config64path;
+            RegistryKey key32 = Registry.LocalMachine.OpenSubKey(steam32);
+            RegistryKey key64 = Registry.LocalMachine.OpenSubKey(steam64);
+            if (key64.ToString() == null || key64.ToString() == "")
+            {
+                foreach (string k32subKey in key32.GetSubKeyNames())
+                {
+                    using (RegistryKey subKey = key32.OpenSubKey(k32subKey))
+                    {
+                        steam32path = subKey.GetValue("InstallPath").ToString();
+                        config32path = steam32path + "/steamapps/libraryfolders.vdf";
+                        string driveRegex = @"[A-Z]:\\";
+                        if (File.Exists(config32path))
+                        {
+                            string[] configLines = File.ReadAllLines(config32path);
+                            foreach (var item in configLines)
+                            {
+                                Console.WriteLine("32:  " + item);
+                                Match match = Regex.Match(item, driveRegex);
+                                if (item != string.Empty && match.Success)
+                                {
+                                    string matched = match.ToString();
+                                    string item2 = item.Substring(item.IndexOf(matched));
+                                    item2 = item2.Replace("\\\\", "\\");
+                                    item2 = item2.Replace("\"", "\\steamapps\\common\\");
+                                    steamGameDirs.Add(item2);
+                                }
+                            }
+                            steamGameDirs.Add(steam32path + "\\steamapps\\common\\");
+                        }
+                    }
+                }
+            }
+            foreach (string k64subKey in key64.GetSubKeyNames())
+            {
+                using (RegistryKey subKey = key64.OpenSubKey(k64subKey))
+                {
+                    var thing = subKey.GetValue("InstallPath");
+                    if (thing == null) return;
+                    steam64path = thing.ToString();
+                    config64path = steam64path + "/steamapps/libraryfolders.vdf";
+                    string driveRegex = @"[A-Z]:\\";
+                    if (File.Exists(config64path))
+                    {
+                        string[] configLines = File.ReadAllLines(config64path);
+                        foreach (var item in configLines)
+                        {
+                            Console.WriteLine("64:  " + item);
+                            Match match = Regex.Match(item, driveRegex);
+                            if (item != string.Empty && match.Success)
+                            {
+                                string matched = match.ToString();
+                                string item2 = item.Substring(item.IndexOf(matched));
+                                item2 = item2.Replace("\\\\", "\\");
+                                item2 = item2.Replace("\"", "\\steamapps\\common\\");
+                                steamGameDirs.Add(item2);
+                            }
+                        }
+                        steamGameDirs.Add(steam64path + "\\steamapps\\common\\");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
